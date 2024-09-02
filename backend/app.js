3d791f0fc461744a6e2230aeaff0f44e94eb0701
sahita412 <neerajsahita@gmail.com>
2024-09-02 14:38:22 +0530

    debugging1

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,15 @@ if (process.env.NODE_ENV !== "production"){
 }
 
 //using middleware
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-  
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+
+app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit:'50mb', extended: true}));
 app.use(cookieParser());
